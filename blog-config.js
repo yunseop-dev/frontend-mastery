9454544c2e78f06088b2487fc0120fcad9a3a88d
Yunseop Kim <c.o.d.e@kakao.com>
2022-09-17 12:04:44 +0900

    add: site verification for search console

--- blog-config.js
+++ blog-config.js
@@ -3,6 +3,7 @@ module.exports = {
   description: "frontend-mastery",
   author: "Yunseop Kim",
   siteUrl: "https://frontend-mastery.web.app",
+  siteVerification: "fhjKymkIfXyW26DnwWdzr5iBr7Q-n2HB8eSlpj7XF9Q",
   links: {
     github: "https://github.com/yunseop-dev",
     email: "mailto:c.o.d.e@kakao.com",
--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import { siteUrl } from "../../../blog-config"
+import { siteUrl, siteVerification } from "../../../blog-config"
 
 const SEO = ({ title, description, url }) => {
   return (
@@ -9,6 +9,7 @@ const SEO = ({ title, description, url }) => {
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       <meta property="og:image" content={`${siteUrl}/og-image.png`} />
+      <meta name="google-site-verification" content={siteVerification} />
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
     </Helmet>
